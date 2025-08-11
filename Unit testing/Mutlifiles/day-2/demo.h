c89ce49d99016171950c331c86ff10137f7bc6d9
ATLA JAGADISHWAR REDDY <jagadish1624@gmail.com>
2025-08-11 11:01:21 +0530

    added hach=kerrank week 2

---
+++ Unit testing/Mutlifiles/day-2/demo.h
@@ -0,0 +1,7 @@
+#ifndef DEMO_H
+#define DEMO_H
+
+int add(int a,int b);
+int square(int a);
+int product(int a,int b);
+#endif
---
+++ Unit testing/Mutlifiles/day-2/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "demo.h"
+int main(){
+    int a=10,b=20,c,d,e;
+    c=add(a,b);
+    d=square(a);
+    e=product(a,b);
+    printf("%d\n",c);
+        printf("%d\n",d);
+    printf("%d\n",e);
+
+
+}
