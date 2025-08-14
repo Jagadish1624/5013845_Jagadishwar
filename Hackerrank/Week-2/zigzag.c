17aacec422a9c834ecfc45af4767b62017e7049c
ATLA JAGADISHWAR REDDY <jagadish1624@gmail.com>
2025-08-14 11:44:05 +0530

    Week-2 hackerrank problems

--- Hackerrank/Week-2/zigzag.c
+++ Hackerrank/Week-2/zigzag.c
@@ -25,7 +25,6 @@ void zigzag(int arr[],int n){
         printf("%d",arr[i]);
     }
 }
-
 int main() {
     int t;
     scanf("%d",&t);
@@ -34,15 +33,9 @@ int main() {
         scanf("%d",&n);
         int arr[n];
         for (int i=0; i<n; i++) {
-        
-        
         scanf("%d",&arr[i]);
         }
-    
-    
     zigzag(arr,n);
-    
     }
-    return 0;
-    
+    return 0;   
 }
